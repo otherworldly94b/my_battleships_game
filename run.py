0e6bfe0f0c21becb92b9c56eb73888945c6cfe44
otherworldly94b <and.charalambous@gmail.com>
2023-07-08 13:01:05 +0000

    Add class and dockstring for what it does

--- run.py
+++ run.py
@@ -1,2 +1,11 @@
 from random import randint
 
+# Create the game board
+# Code from CI sample video
+class Board:
+    """
+    Creates the main board. Sets the size and the number 
+    of ships allowed and the player's name.
+    There are methods that print the board, add the ships
+    and the guess on the board
+    """
